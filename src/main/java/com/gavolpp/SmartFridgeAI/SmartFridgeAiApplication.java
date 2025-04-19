2b34604afac6d8cfaa756c5a8985e0284c4dd4a3
gavolpp <volpatog3@gmail.com>
2025-04-19 10:59:36 -0300

    Spring initializr

---
+++ src/main/java/com/gavolpp/SmartFridgeAI/SmartFridgeAiApplication.java
@@ -0,0 +1,13 @@
+package com.gavolpp.SmartFridgeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFridgeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gavolpp/SmartFridgeAI/SmartFridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavolpp.SmartFridgeAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFridgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
